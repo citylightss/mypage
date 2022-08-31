87c46542c9e3b2f2912f7f2aa8eea20114b86f3e
Irina V <109794332+citylightss@users.noreply.github.com>
2022-08-31 20:35:13 +0300

    javascript

---
+++ script.js
@@ -0,0 +1,35 @@
+let intro = document.querySelector(".intro");
+let details = document.querySelectorAll("details");
+let upButton = document.querySelector(".up-button");
+
+function showIntro() {
+	intro.classList.add("intro-show");
+}
+setInterval(showIntro(), 1000);
+
+for (i = 0; i < details.length; i++) {
+	details[i].addEventListener("toggle", hideText);
+}
+
+function hideText(event) {
+	if (!event.target.open) return;
+	let details = event.target.parentNode.children;
+	for (i = 0; i < details.length; i++) {
+		if (
+			details[i].tagName != "DETAILS" ||
+			!details[i].hasAttribute("open") ||
+			event.target == details[i]
+		) {
+			continue;
+		}
+		details[i].removeAttribute("open");
+	}
+}
+
+$(".gallery-list").magnificPopup({
+	delegate: "a",
+	type: "image",
+	gallery: {
+		enabled: true,
+	},
+});
